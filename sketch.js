dc4ccbfc5ae7342dd09cc44faed1f67744014317
Zoe Valladares <zvalladares@wlapcs.org>
2017-03-23 20:42:00 +0000

    still working on the images -- harder then expected

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ var x = 300;
 var y = 200;
 var speedx = 8;
 var speedy = 6;
+var diameter = 50;
 
 
 function setup(){
@@ -10,18 +11,26 @@ function setup(){
 
 function draw(){
     background(0);
+    display();
+    move();
+    bounce();
+}
 
+function display(){
     //display ball
     stroke(255);
     strokeWeight(4);
     fill(50,250,50);
-    ellipse(x, y, 50, 50);
+    var ball = ellipse(x, y, diameter, diameter);
+}
 
-    
-    //make ball move
+function move(){
+     //make ball move
     y = y + speedy;
     x = x + speedx;
-    
+}
+
+function bounce(){
     //ball changes direction when it hits wall
     if(x>600 || x<0){
         speedx = -speedx;
@@ -29,4 +38,25 @@ function draw(){
      if(y>400 || y<0){
         speedy = -speedy;
     }
+   
 }
+
+ function keyPressed() {
+  if (keyCode == LEFT_ARROW) {
+    diameter = 255;
+    
+    
+  } else if (keyCode == RIGHT_ARROW) {
+    diameter = 10;
+  }
+  if (keyCode == UP_ARROW){
+      diameter = 30;
+  }else if (keyCode == DOWN_ARROW){
+      diameter = 75;
+  }
+  if (keyCode == space){
+      ellipse(x, y, 20, 50);
+  }
+ }
+ 
+ 
